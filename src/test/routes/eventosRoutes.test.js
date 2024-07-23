456b7ad63f4835aac4f18cc013ac022c9098e63a
eduardo <jose.eduardo@inbec.edu.br>
2024-07-23 11:05:46 -0300

    feat: :triangular_flag_on_post: add: feature flang usando unleash para /eventos

--- src/test/routes/eventosRoutes.test.js
+++ src/test/routes/eventosRoutes.test.js
@@ -2,11 +2,14 @@
 import { after } from 'mocha';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import sinon from 'sinon';
 import app from '../../app.js';
 import db from '../../db/dbconfig.js';
+import EventosController from '../../controllers/eventosController.js';
 
 chai.use(chaiHttp);
 const { expect } = chai;
+let stub;
 
 after(async () => {
   await db.destroy();
@@ -14,7 +17,7 @@ after(async () => {
 
 describe('GET em /eventos', () => {
   it('Deve retornar uma lista de eventos', (done) => {
-    process.env.EVENTO_FLAG = 'true';
+    stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(true);
     chai.request(app)
       .get('/eventos')
       .set('Accept', 'application/json')
@@ -29,7 +32,7 @@ describe('GET em /eventos', () => {
   });
 
   it('Deve retornar erro 404', (done) => {
-    process.env.EVENTO_FLAG = 'false';
+    stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(false);
     chai.request(app)
       .get('/eventos')
       .set('Accept', 'application/json')
